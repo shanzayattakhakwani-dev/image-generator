6f5e1ad0547dd0e9c308e924313b0291e9efef05
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-02 22:26:34 +0500

    Shanzay-BSE233043

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {assets} from '../assets'
+
+ const Navbar = () => {
+  return (
+    <div>
+        <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'/>
+    </div>
+  )
+}
+export default Navbar

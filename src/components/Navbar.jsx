00b5e96701838c2b081c81ec7cf90cae75d003cb
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-02 22:47:25 +0500

    Shanzay-BSE233043

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react'
 import { assets } from "../assets/assets";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const [user, setUser] = useState(null);
 
+const navigate = useNavigate()
+
   return (
-    <div>
+    <div className='flex items-center justify-between py-4'>
       <Link to='/'>
         <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40' />
       </Link>
 
       <div>
         {user ? (
-          <div></div>
-        ) : (
           <div>
-            <p>Pricing</p>
-            <button>Login</button>
+
+          </div>
+        ) : (
+          <div className='flex items-center gap-2 sm:gap-5'>
+            <p onClick={()=>navigate('/buy')}
+            className='cursor-pointer'>Pricing</p>
+            <button className='bg-zinc-800 text-white px-7 py-2 sm:py-2 text-sm rounded-full'>Login</button>
           </div>
         )}
       </div>

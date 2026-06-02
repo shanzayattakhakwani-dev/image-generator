2ccdfdd366eec72d35dfce26a63967eda8684aaf
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-02 22:07:31 +0500

    Shanzay-BSE233043

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
@@ -6,9 +7,11 @@ import BuyCredit from './pages/BuyCredit'
 const App = () => {
   return (
     <div>
-      <Home/>
-      <Result/>
-      <BuyCredit/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
     </div>
   )
 }

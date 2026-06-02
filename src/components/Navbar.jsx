bf24b5675e3d9bc4db6b1dd2c8f3c9ea11783bc2
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-02 22:40:51 +0500

    Shanzay-BSE233043

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from "../assets/assets";
- const Navbar = () => {
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const [user, setUser] = useState(null);
+
   return (
     <div>
-        <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'/>
+      <Link to='/'>
+        <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40' />
+      </Link>
+
+      <div>
+        {user ? (
+          <div></div>
+        ) : (
+          <div>
+            <p>Pricing</p>
+            <button>Login</button>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
+
 export default Navbar

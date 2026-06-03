f9fd3e80ec882e7ac96c6bdb360cc3c48dfa12c2
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-03 10:22:02 +0500

    Shanzay-BSE233043

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,15 @@ const Header = () => {
             </div>
     
     <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>
-        Turn text to image, in seconds.
+        Turn text to<span className='text-blue-600'>image </span> , in seconds.
     </h1>
+    <p className='text-center max-w-xl mx-auto mt-5'>
+        Unleah your creativity with AI. Turn your imagination into visual art in seconds-just type,and watch the magic happen.
+    </p>
+    <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'>
+        Generate Images
+<img className='h-6' src={assets.star_group} alt=""/>
+    </button>
     </div>
   );
 };

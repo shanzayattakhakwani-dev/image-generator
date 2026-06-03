410492b90358c9435d53b35d638d12e7efac435b
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-03 20:26:13 +0500

    Shanzay-BSE233043-changed state between login and signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,23 @@
+import { useState } from 'react'
 import {assets} from '../assets/assets'
 
 const Login = () => {
-  return (
+const [state, setState]= useState('Login')
+
+
+    return (
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10
     backdrop-blur-sm bg-black/30 flex justify-center items-center'>
 
       <form className='relative bg-white p-10 rounded-xl
       text-slate-500'>
-        <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
+        <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
         <p className='text-sm'>Welcome back! Please sign in to continue</p>
 
-        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+        {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
           <img src={assets.user_icon} alt="" />
           <input type="text" className='outline-none text-sm' placeholder='Full Name' required/>
-        </div>
+        </div>}
          <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
           <img src={assets.email_icon} alt="" />
           <input type="email" className='outline-none text-sm' placeholder='Email id' required/>
@@ -26,13 +30,15 @@ const Login = () => {
 cursor-pointer'>Forgot password?</p>
 
 <button className='bg-blue-600 w-full text-white py-2
-rounded-full'>create account</button>
-     <p className='mt-5 text-center'>Don't have an account? 
-<span className='text-blue-600 cursor-pointer'>Sign up</span></p>
-
+rounded-full'>{state==='Login'? 'Login' : 'Create account'}</button>
+    
+    { state=='Login' ? <p className='mt-5 text-center'>Don't have an account? 
+<span className='text-blue-600 cursor-pointer' onClick={()=>setState('Sign Up')}>Sign up</span></p>
+:
 <p className='mt-5 text-center'>Already have an account? 
-<span className='text-blue-600 cursor-pointer'>Login</span></p>
-     
+<span className='text-blue-600 cursor-pointer'onClick={()=>setState('Login')}>Login</span></p>}
+     <img src={assets.cross_icon} alt="" className='absolute 
+top-5 right-5 cursor-pointer'/>
       </form>
 
     </div>

552258a1f66678d3de4c0025d82bdb200eaacaf9
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-03 19:47:55 +0500

    Shanzay-BSE233043-working on BuyCredit page now

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const BuyCredit = () => {
   return (
-    <div>BuyCredit</div>
+    <div className='min-h-[80vh] text-center pt-14
+    mb-10'>
+        <button className='border border-gray-400 px-10
+        py-2 rounded-full mb-6'>Our Plans</button>
+        <h1 className='text-center text-3xl font-medium
+        mb-6 sm:mb-10'>Choose the plan</h1>
+    </div>
   )
 }
 

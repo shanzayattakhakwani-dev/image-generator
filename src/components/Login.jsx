8a768c714b6027c24df231380eaa465528f68051
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-03 20:04:05 +0500

    Shanzay-BSE233043-Basics of login page done and added input field for name

---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='absolute top-0 left-0 right-0 bottom-0 z-10
+    backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+
+      <form>
+        <h1>Sign Up</h1>
+        <p>Welcome back! Please sign in to continue</p>
+      </form>
+
+    </div>
+  )
+}
+
+export default Login

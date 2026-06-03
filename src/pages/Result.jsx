6d653ca8f730a9f4e1dd606a7074c3e26bd33c60
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-03 11:53:46 +0500

    Shanzay-BSE233043-working on result page now

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -14,6 +14,10 @@ const Result = () => {
 <input type="text" placeholder='Describe what tou want to generate' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20'/>
 <button type='submit' className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'>Generate</button>
       </div>
+      <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
+        <p className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Genearte Another</p>
+        <a href="" download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>Downloade</a>
+      </div>
     </div>
     </form>
   )

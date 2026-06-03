2e1a1e7e4bb594de3b0a6cc1d5da70648a93b90d
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-03 20:13:48 +0500

    Shanzay-BSE233043-add name email and passowrd firlds

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,12 +7,20 @@ const Login = () => {
 
       <form className='relative bg-white p-10 rounded-xl
       text-slate-500'>
-        <h1>Sign Up</h1>
-        <p>Welcome back! Please sign in to continue</p>
+        <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
+        <p className='text-sm'>Welcome back! Please sign in to continue</p>
 
-        <div>
+        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
           <img src={assets.user_icon} alt="" />
-          <input type="text" placeholder='Full Name' required/>
+          <input type="text" className='outline-none text-sm' placeholder='Full Name' required/>
+        </div>
+         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+          <img src={assets.email_icon} alt="" />
+          <input type="email" className='outline-none text-sm' placeholder='Email id' required/>
+        </div>
+         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+          <img src={assets.lock_icon} alt="" />
+          <input type="password" className='outline-none text-sm' placeholder='Password' required/>
         </div>
       </form>
 

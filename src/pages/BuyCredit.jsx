330f86d65fe985eddb3a55d8912bedd7bb389115
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-03 19:55:23 +0500

    Shanzay-BSE233043-added css to the plans

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { assets,plans } from '../assets/assets'
 const BuyCredit = () => {
   return (
     <div className='min-h-[80vh] text-center pt-14
@@ -8,6 +8,18 @@ const BuyCredit = () => {
         py-2 rounded-full mb-6'>Our Plans</button>
         <h1 className='text-center text-3xl font-medium
         mb-6 sm:mb-10'>Choose the plan</h1>
+    
+    <div className='flex flex-wrap justify-center gap-6 text-left'>
+  {plans.map((item, index)=>(
+    <div key={index}
+    className='bg-white drop-shadow-sm border rounded-lg py-12 px-8 text-gray-600 hover:scale-105 transition-all duration-500'>
+      <img width={40} src={assets.logo_icon} alt="" />
+      <p className='mt-3 mb-1 font-semibold'>{item.id}</p>
+      <p className='text-sm'>{item.desc}</p>
+      <p className='mt-6'><span className='text-3xl font-medium'>{item.price} </span>/ {item.credits} credits</p>
+    </div>
+  ))}
+</div>
     </div>
   )
 }

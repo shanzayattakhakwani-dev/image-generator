f3510eee3479ab6f6247fe3f5a36adb43f624fb2
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-08 21:02:44 +0500

    added farmer motion to BuyCredit and Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from 'react'
 import {assets} from '../assets/assets'
 import { AppContext } from '../context/AppContext'
+import { motion } from "framer-motion";
 
 const Login = () => {
 const [state, setState]= useState('Login')
@@ -16,7 +17,11 @@ useEffect(()=>{
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10
     backdrop-blur-sm bg-black/30 flex justify-center items-center'>
 
-      <form className='relative bg-white p-10 rounded-xl
+      <motion.form initial={{ opacity: 0.2, y: 50 }}
+transition={{ duration: 0.3 }}
+whileInView={{ opacity: 1, y: 0 }}
+viewport={{ once: true }}
+      className='relative bg-white p-10 rounded-xl
       text-slate-500'>
         <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
         <p className='text-sm'>Welcome back! Please sign in to continue</p>
@@ -46,7 +51,7 @@ rounded-full'>{state==='Login'? 'Login' : 'Create account'}</button>
 <span className='text-blue-600 cursor-pointer'onClick={()=>setState('Login')}>Login</span></p>}
      <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" className='absolute 
 top-5 right-5 cursor-pointer'/>
-      </form>
+      </motion.form>
 
     </div>
   )

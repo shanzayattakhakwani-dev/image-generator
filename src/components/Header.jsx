cfba22399643a5e63565f7f7ccdd779bf1ce9d97
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-08 20:39:48 +0500

    Redirect to results if logged in, otherwise show login modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import { assets } from '../assets/assets';
 import {motion} from "framer-motion";
 const Header = () => {
+
+  const {user, setShowLogin} = useContext(AppContext)
+  const navigate = useNavigate()
+
+  const onClickHandler = () =>{
+    if(user){
+      navigate('/result')
+    }else{
+      setShowLogin(true)
+    }
+  }
+
   return (
     <motion.div className='flex flex-col justify-center items-center text-center my-20'
     initial={{opacity:0.2, y:100}}
@@ -32,7 +44,7 @@ animate={{ opacity: 1 }}
 transition={{ delay: 0.4, duration: 2 }}>
         Unleah your creativity with AI. Turn your imagination into visual art in seconds-just type,and watch the magic happen.
     </motion.p>
-    <motion.button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
+    <motion.button onClick={onClickHandler} className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
     whileHover={{ scale: 1.05 }}
 whileTap={{ scale: 0.95 }}
 initial={{ opacity: 0 }}

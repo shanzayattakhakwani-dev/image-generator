02b9db02de531e8f59a7302293b44598e4eedae6
Shanzayattakhakwani-dev <Shanzayattakhakwani@gmail.com>
2026-06-11 21:31:09 +0500

    feat: complete clipdrop integration and resolve 401 unauthorized api key issue

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 const PORT = process.env.PORT || 4000
 const app = express()
 
-// 1. Middlewares (Declare only once)
+
 app.use(express.json())
 app.use(cors())
 
@@ -17,5 +18,7 @@ await connectDB()
 // 3. Routes
 app.get('/', (req, res) => res.send("API Working"))
 app.use('/api/user', userRouter)
-
+app.use('/api/image', imageRouter) 
+// Add this temporarily at the bottom of server.js to test your environment configuration:
+console.log("Your Loaded Clipdrop Key is:", process.env.CLIPDROP_API);
 app.listen(PORT, () => console.log('Server running on port ' + PORT))
